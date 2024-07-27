8bbfd9f4aac7b50a50b4944bf6a474cf34ba18f4
Muhammad Jaffar <muhammadjaffar0075@gmail.com>
2024-07-27 09:44:49 +0500

    more fixed for mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,8 @@ const swiperNav = new Swiper('.nav-menu-bottom', {
     spaceBetween: 5,
 });
 
+// dropdowns
+
 // Toggle dropdown menu on button click
 $("#navbar-bottom .nav-menu-bottom .toggle-dropdown-menu").click(function (event) {
     $(".category-dropdown-menu").fadeToggle();
@@ -45,32 +47,40 @@ $("#moreMenu").click(function (event) {
 
 
 // mobile menu 
-
 $("#toggleMobileMenu").click(function () {
     $("#mobileMenu").slideToggle(200);
     $("#mobileMenu").toggleClass("toggled");
     $(this).find("i").toggleClass("fa-close fa-bars");
 })
 
-
 $("#mobileMenu .mobile-nav-menu .mobile-nav-menu-heading").click(function () {
     $(this).parent().find(".mobile-sub-menu").toggle(200);
     $(this).find("i").toggleClass("fa-caret-up fa-caret-down");
 })
 
-
+// handle scrolling
 var $header = $('header');
 var $mobileMenu = $('header #mobileMenu');
 var $mobileMenuToggle = $('#toggleMobileMenu i');
 var lastScrollTop = 0;
 
 function handleScroll() {
-    var currentScrollTop = $(window).scrollTop();
+
     var isMenuToggled = $mobileMenu.hasClass('toggled');
+
+    var currentScrollTop = $(window).scrollTop();
 
     if ($(window).width() > 700 && currentScrollTop >= 600) {
         $header.addClass('fixed');
     } else if ($(window).width() < 700) {
+
+
+        if (isMenuToggled) {
+            $header.addClass('show-header fixed-mobile');
+            return;
+        }
+
+
         if (currentScrollTop >= 300) {
             $header.addClass('fixed-mobile');
             if (currentScrollTop < lastScrollTop) {
